a1ebd4514ea01f7c249dde17bd31f4dd34fa16b0
larawarwick <83228846+larawarwick@users.noreply.github.com>
2022-01-26 21:38:10 +0000

    added larawarwickk.py
    Test File

---
+++ larawarwickk.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+# ls lists files in current directory(folder)
+# cd is change directory, e.g cd foldername
+# run python file: python3 nameoffile.py
+# command s for save in order for changes to be applied
